20e878122f37650211a5d5fddbff2f195755d882
stijnvdmeer <stijn08072002@outlook.com>
2024-12-13 16:37:05 +0100

    Opdracht 1b

--- scripts/main.js
+++ scripts/main.js
@@ -73,3 +73,4 @@ const departments = {
 
 // console.log(departments);
 console.log("De afdeling Sales heeft " + departments.sales.numberOfEmployees + " medewerkers.");
+console.log("Marketing is een leuke afdeling om te werken. " + departments.marketing.description);
